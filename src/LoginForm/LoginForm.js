fd90b4fa4be7de1d85065fedf4843bcfe1d89880
Sooseki <bonnetmanon3006@gmail.com>
2022-06-01 16:12:57 +0200

    login form front

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -1,12 +1,17 @@
 import './LoginForm.css';
+import { useState } from 'react';
 
 export function LoginForm() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="LoginForm">
       <h1 className="LoginForm__title">Connexion</h1>
       <form>
-        <input type="text" name="username" placeholder="Pseudo" />
-        <input type="password" name="password" placeholder="Mot de passe" />
+        <input type="text" name="username" placeholder="Pseudo" value={this.state.value} onChange={this.setUsername} />
+        <input type="password" name="password" placeholder="Mot de passe" value={this.state.value} onChange={this.setPassword} />
       </form>
     </div>
   );
