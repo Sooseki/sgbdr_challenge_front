42e5b31b24a44d8db2e96bcb76b3e1ba3c14a73a
Robin <robinpinondev@gmail.com>
2022-06-24 11:11:36 +0200

    Working form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,74 +1,77 @@
-import './form.css';
+import "./form.css";
 import { useState } from "react";
 
 export const Form = () => {
-    const [name, setName] = useState("");
-    const [firstName, setFirstName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-    
-    let handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-          let res = await fetch("http://localhost:5050/register/mt4/sgbdr", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json, text/plain, /',
-                'Content-Type': 'application/json'
-              },
-            body: JSON.stringify({
-            "name_student": name,
-            "first_name_student": email,
-            "email_student": firstName,
-            }),
-           
-          });
-          
-          let resJson = await res.json();
-          console.log(resJson);
-          if (res.status === 200) {
-            setName("");
-            setEmail("");
-            setFirstName("");
-            setMessage("User created successfully");
-          } else {
-            setMessage("Some error occured");
-          }
-        } catch (err) {
-          console.log(err);
-        }
-      };
+  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-    return (
-        <div className="App">
-          <form onSubmit={handleSubmit}>
+  let handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      let res = await fetch("http://localhost:5050/register/mt4/sgbdr", {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, /",
+          "Content-Type": "application/json"
+          // pour la prochaine requete
+          // Authorization: "Bearer " + localStorage.getItem("token")
+        },
+        body: JSON.stringify({
+          name_student: name,
+          first_name_student: email,
+          email_student: firstName
+        })
+      });
 
-            <input
-              type="text"
-              value={name}
-              placeholder="Name"
-              onChange={(e) => setName(e.target.value)}
-            />
-             <input
-              type="text"
-              value={firstName}
-              placeholder="Firstname"
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-            <input
-              type="text"
-              value={email}
-              placeholder="Email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-           
-    
-            <button type="submit">S'identifier</button>
-    
-            <div className="message">{message ? <p>{message}</p> : null}</div>
-          </form>
-        </div>
-      );
+      const resJson = await res.json();
+
+      console.log(resJson);
+
+      if (res.status === 200) {
+        localStorage.setItem("token", resJson);
+        setName("");
+        setEmail("");
+        setFirstName("");
+        setMessage("User created successfully");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          onChange={e => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={firstName}
+          placeholder="Firstname"
+          onChange={e => setFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={email}
+          placeholder="Email"
+          onChange={e => setEmail(e.target.value)}
+        />
+        <button type="submit">S'identifier</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
+    </div>
+  );
+};
+
+{
+  /* <input className='questions' type={questions[key].tag} onChange={(e) => setData(e.target.value)}/> */
 }
-
-{/* <input className='questions' type={questions[key].tag} onChange={(e) => setData(e.target.value)}/> */ }
