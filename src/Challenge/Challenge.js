5891f1f84382cc35e486cc3a6db69d58e193f20d
Sooseki <bonnetmanon3006@gmail.com>
2022-07-11 10:19:23 +0200

    wah

--- src/Challenge/Challenge.js
+++ src/Challenge/Challenge.js
@@ -8,24 +8,24 @@ import ChallengeTests from './ChallengeTests/ChallengeTests';
 
 const Challenge = () => {
 
-    // const [getChallenge, challenge, setChallenge] = useGetApi("http://localhost:5050//admin/student")
+    const [getChallenge, challenge, setChallenge] = useGetApi("http://localhost:5050/admin/student/1")
     const [selectedMenu, setSelectedMenu] = useState('login-form');
-    const [challenge, setChallenge] = useState({
-      id_challenge: 1,
-      id_student: 1,
-      points: {
-        has: 4,
-        max: 20
-      },
-      current: {
-        number: 2,
-        nb_points: 3,
-        label: "Eh ouais ?",
-        error: "Wallah c'est pas bon",
-        clue: "Wallah trouve la solution"
-      },
-      passed: 1,
-    });
+    // const [challenge, setChallenge] = useState({
+    //   id_challenge: 1,
+    //   id_student: 1,
+    //   points: {
+    //     has: 4,
+    //     max: 20
+    //   },
+    //   current: {
+    //     number: 2,
+    //     nb_points: 3,
+    //     label: "Eh ouais ?",
+    //     error: "Wallah c'est pas bon",
+    //     clue: "Wallah trouve la solution"
+    //   },
+    //   passed: 1,
+    // });
 
     useEffect(() => {
         getChallenge()
