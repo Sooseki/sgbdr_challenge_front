7a34f9e8babd61bd1448889c1ef281d026949e53
David <david.dehe.pro@gmail.com>
2022-07-11 22:32:46 +0200

    fix question and api call data

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -22,7 +22,7 @@ export const usePostApi = (_url, data) =>  {
   const postApi = async (data) => {
     try {
       console.log(data)
-      const postedData = await axios.post(_url, {data})
+      const postedData = await axios.post(_url, data)
       return postedData
     } catch (error) {
       return false
--- src/Challenge/LoginForm/LoginForm.js
+++ src/Challenge/LoginForm/LoginForm.js
@@ -5,6 +5,7 @@ import { Form } from "../../components/Form";
 const LoginForm = () => {
   const [data, setData] = useState({
     name_student: 'David',
+    first_name_student: 'Dehe',
     email_student: 'dehe@dehe'
   })
 
@@ -14,12 +15,16 @@ const LoginForm = () => {
       tag: 'name_student'
     },
     q2: {
+      question: 'prénom de létudiant',
+      tag: 'first_name_student'
+    },
+    q3: {
       question: 'email de létudiant',
       tag: 'email_student'
     }
   }
   
-  const [getStudent] = usePostApi('http://localhost:5050/admin/student')
+  const [getStudent] = usePostApi('http://localhost:5050/auth/student')
 
   const useSubmit = async() => {
     console.log(getStudent)
