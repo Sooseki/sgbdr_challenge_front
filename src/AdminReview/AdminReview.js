55452193ecdca661f1a83cc91864844b0513fc56
David <david.dehe.pro@gmail.com>
2022-07-13 00:03:15 +0200

    admin review now clean

---
+++ src/AdminReview/AdminReview.js
@@ -0,0 +1,37 @@
+
+import { useGetApi } from '../Api/Api'
+import { useState, useEffect } from 'react';
+import "./AdminReview.css"
+
+
+const AdminReview = () => {
+
+    const [getStudent, students, setStudents] = useGetApi("http://localhost:5050/admin/student")
+
+    useEffect(() => {
+        getStudent()
+    }, []);
+
+    return (
+        <div>
+            <ul className='test title'>
+                <li>id_student</li>
+                <li>id_challenge</li>
+                <li>name_student</li>
+                <li>email_student</li>
+                <li>first_name_student</li>
+            </ul>
+            {students && students.rows.map((student, index) => (
+                <ul key={`${student.name_student}-${index}`} className={`test${index} test`}>
+                    <li>{student.id_student}</li>
+                    <li>{student.id_challenge}</li>
+                    <li>{student.name_student}</li>
+                    <li>{student.email_student}</li>
+                    <li>{student.first_name_student}</li>
+                </ul>
+            )
+            )}
+        </div>
+    );
+}
+export default AdminReview
